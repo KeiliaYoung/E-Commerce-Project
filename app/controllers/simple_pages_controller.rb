1fb1a3b5de837c2fc822b364275df204742c63d5
Keilia Young <keiliayoung@gmailL.com>
2018-11-01 14:17:28 +0000

    4.5 Views

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,9 +1,4 @@
 class SimplePagesController < ApplicationController
-  def index
-  end
-end
-
-class SimplePagesController < ApplicationController
   def landing_page
     @products = Product.limit(3)
   end
