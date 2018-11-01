919af19dc3403bfe44fcd7a82accc4231a9e5465
Keilia Young <keiliayoung@gmailL.com>
2018-11-01 20:09:04 +0000

    4.6 Orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.create(name: "Decorative-Vintage", image_url: "https://images.homedepot-static.com/productImages/10c02d87-46ac-454b-8bbd-eeb475d12bf4/svn/gray-litton-lane-plant-pots-44472-64_1000.jpg", description: "Decorative vintage bike with basket.", price: "decimal")
-Product.create(name: "Classic-Vintage", image_url: "https://c.stocksy.com/a/fqB000/z9/45549.jpg", description: "Classic vintage bike with basket.", price: "decimal")
-Product.create(name: "Boho-Vintage", image_url: "https://i.pinimg.com/originals/ba/d9/d0/bad9d0b2ed2fd0888fff6d48e00a1265.jpg", description: "Boho vintage bike with basket.", price: "decimal")
+
+
+Order.create(user_id: 1, product_id: 1, total: 10.0)
+Order.create(user_id: 1, product_id: 2, total: 5.0)
