12f7e91decc50eddf66cee5afc042cf280078601
Keilia Young <keiliayoung@gmail.com>
2018-11-13 17:26:08 -0500

    Ex 5.1 Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  get 'simple_pages/landing_page', to: 'products#index'
+  get 'simple_pages/landing_page', to: 'products#index00000000000'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'simple_pages#about'
   resources :orders, only: [:index, :show, :create, :destroy]
