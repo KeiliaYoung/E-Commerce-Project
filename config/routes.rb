51862226fdcf96e52ea3a60bb75623c433b9cf8b
Khelii <keiliayoung@gmail.com>
2019-01-23 02:51:11 -0500

    Ex. 6.4 Generating Test Data (FactoryBot)

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
+  get 'simple_pages/landing_page' #, to: 'products#index'
     root 'simple_pages#landing_page'
   post 'simple_pages/thank_you'
 
